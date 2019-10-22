fd84bbf24e0777227be6c624a113cd9ba1ca49af
Nandini Narinesingh <Nandini.Narinesingh@myguardiangroup.com>
2019-10-22 14:11:07 -0400

    Updates

---
+++ precache-manifest.c3f735882197accd748c45c0e69b9c5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0400d67c6f2ef86480863b6e531ccb10",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "89bf9aab880248b89d11",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "60dc99aff456c2b82a00",
+    "url": "/robofriends/static/css/main.3975f368.chunk.css"
+  },
+  {
+    "revision": "89bf9aab880248b89d11",
+    "url": "/robofriends/static/js/2.a17edf5f.chunk.js"
+  },
+  {
+    "revision": "60dc99aff456c2b82a00",
+    "url": "/robofriends/static/js/main.63ec2974.chunk.js"
+  },
+  {
+    "revision": "fa64f21900e25e9ebf3b",
+    "url": "/robofriends/static/js/runtime~main.2c7325ee.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
